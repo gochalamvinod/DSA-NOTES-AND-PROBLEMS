431b2703fb8eb314c74a7b763034f3c1c3d6ba8e
gochalamvinod <gochalamannavinod@gmail.com>
2026-08-28 18:25:17 +0530

    level2

--- RECURSION (FOR BASIC DSA)/MainLevel2.java
+++ RECURSION (FOR BASIC DSA)/MainLevel2.java
@@ -26,13 +26,32 @@ class Reverse{
     }
 }
 
-
+class Palandrome{
+    public static boolean check(String S){
+        return S.charAt(0)==S.charAt(S.length()-1);
+    }
+    public static boolean output(int n){
+        if (n>0 && n<=9){
+            return true;
+        }
+        else if (check(Integer.toString(n))){
+            if(n%10==n/10){
+                return true;
+            }
+            return output(Integer.parseInt(Integer.toString(n).substring(1, Integer.toString(n).length() - 1)));
+        }
+        else{
+            return false;
+        }
+    }
+}
 
 
 public class MainLevel2{
     public static void main(String args[]){
         // System.out.print(Count.output(807465));
         // System.out.print(Reverse.output(807465));
+        System.out.print(Palandrome.output(11));
 
 
     }
