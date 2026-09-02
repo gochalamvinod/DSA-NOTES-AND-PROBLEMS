a93708e50d4f8718f40113181a01f139a47c9fea
gochalamvinod <gochalamannavinod@gmail.com>
2026-09-02 22:16:51 +0530

    Practice

---
+++ DSA ALL TYPES BASICS/tempCodeRunnerFile.java
@@ -0,0 +1,36 @@
+class tempCodeRunnerFile {
+
+    public static int value(int[] arr, int i, int j, int target) {
+
+        // Base case: target not found
+        if (i > j) {
+            return -1;
+        }
+
+        int mid = (i + j) / 2;
+
+        // Target found
+        if (arr[mid] == target) {
+            return mid;
+        }
+
+        // Search right half
+        else if (arr[mid] < target) {
+            return value(arr, mid + 1, j, target);
+        }
+
+        // Search left half
+        else {
+            return value(arr, i, mid - 1, target);
+        }
+    }
+
+    public static void main(String args[]) {
+
+        int[] array = new int[]{
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 200
+        };
+
+        System.out.print(value(array, 0, array.length - 1, 6));
+    }
+}
